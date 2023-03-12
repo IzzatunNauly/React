9e20c42c1d10d2bb5c659327dad60fd2213fa143
IzzatnNauly <naulizzatun159@gmail.com>
2023-03-12 22:29:12 +0700

    State

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import MyButton from './components/MyButton';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import NavbarJs from './components/NavbarJs';
+import { useState } from 'react';
 
 const App = () => {
    const cliked = () => {
@@ -19,14 +21,22 @@ const App = () => {
     )
    }
 
+   const [getNavbarValue, setNavbarValue] = useState("");
+   const changeNavbarValue = () => {
+    setNavbarValue('My Contact');
+   }
+
   return (
     <div className="App">
       <header className="App-header">
+        {/* <NavbarJs /> */}
         <Navbar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is Izzatun Nauly
         </p>
+        {/* {getNavbarValue}
+        <button onClick={() => changeNavbarValue()}>Ubah Navbar</button> */}
         <MyButton cliked={cliked} />
         <Footer paragraf={paragraf} />
       </header>
